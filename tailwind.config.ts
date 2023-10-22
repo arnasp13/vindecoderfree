b4811665db8c66b76c292b3119a8560281dae68f
Arnas <arnaspuidokas6@gmail.com>
2023-10-22 13:11:50 +0300

    all

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    variants: {
+      display: ["group-hover"],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        display: "General Sans",
+        body: "General Sans",
+        GeneralSans: "General Sans",
+      },
+      colors: {
+        "primary-1": "#0061ff",
+        "primary-2": "#104cca",
+        "secondary-1": "#104cca",
+        "secondary-2": "#f2f5ff",
+        "secondary-3": "#ffde6a",
+        "secondary-4": "#fffefa",
+        "secondary-5": "#ff6969",
+        "neutral-800": "#171728",
+        "neutral-700": "#33334e",
+        "neutral-600": "#686781",
+        "neutral-500": "#8c8c9e",
+        "neutral-400": "#aeaec4",
+        "neutral-300": "#e4e4ed",
+        "neutral-200": "#fafafa",
+        "neutral-100": "white",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
