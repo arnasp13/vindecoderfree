8a4f76e04313d42c296bc4cf2d8947eeffd4224c
Arnas <arnaspuidokas6@gmail.com>
2023-10-22 16:21:32 +0300

    update

--- src/components/search-block.tsx
+++ src/components/search-block.tsx
@@ -1,17 +1,18 @@
 "use client";
-import Image from "next/image";
 import React, { FC, useState } from "react";
 import navigation from "next/navigation"; // Import useRouter for navigation
+import { useRouter } from 'next/navigation'
 
 export const SearchBlock: FC<{ placeholder?: string; maxW?: string }> = ({
   placeholder = "Enter your VIN",
   maxW = "600px",
 }) => {
   const [vin, setVin] = useState(""); // State to store the VIN value
+  const router = useRouter();
 
   const handleDecodeClick = () => {
     if (vin) {
-      navigation.useRouter().push(`/vin/${vin}`); // Navigate to the /vin/{vin} page
+      router.push(`/vin/${vin}`); // Navigate to the /vin/{vin} page
     } else {
       alert("Please enter a VIN to decode.");
     }
@@ -26,13 +27,6 @@ export const SearchBlock: FC<{ placeholder?: string; maxW?: string }> = ({
         value={vin} // Bind the value of the input to the vin state
         onChange={(e) => setVin(e.target.value)} // Update the vin state when the input changes
       />
-      {/* <Image
-        width={12}
-        height={12}
-        src="/icons/search.svg"
-        alt="search icon"
-        className="absolute top-1/2 transform left-6 -translate-y-1/2  h-6 w-6"
-      /> */}
       <button
         className="absolute top-1/2 transform right-6 -translate-y-1/2 font-medium bg-primary-1 p-3 text-white rounded-[100px] focus:outline-none"
         onClick={handleDecodeClick} // Add the onClick handler to the button
