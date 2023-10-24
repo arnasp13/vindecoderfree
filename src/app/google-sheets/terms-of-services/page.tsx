83fc287a4c019146834954f18dd3fca8a715d2b7
Arnas <arnaspuidokas6@gmail.com>
2023-10-24 13:10:26 +0300

    update

--- src/app/google-sheets/terms-of-services/page.tsx
+++ src/app/google-sheets/terms-of-services/page.tsx
@@ -1,37 +1,58 @@
 import Head from 'next/head';
 
-const PrivacyPolicy = () => {
+const TermsOfService = () => {
     return (
         <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
             <Head>
-                <title>Privacy Policy for VinoVista: VIN Insight 🚗✨</title>
+                <title>Terms of Service for VinoVista: VIN Insight</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <div className="max-w-3xl mx-auto bg-white p-8 rounded-lg shadow-md">
-                <h1 className="text-2xl font-bold mb-8">Privacy Policy for VinoVista: VIN Insight 🚗✨</h1>
-
-                <p className="mb-6">Last Updated: [Insert Date]</p>
+                <h1 className="text-2xl font-bold mb-8">Terms of Service for VinoVista: VIN Insight</h1>
 
                 <section className="mb-8">
-                    <h2 className="text-xl font-semibold mb-4">1. Introduction</h2>
-                    <p>Welcome to the Privacy Policy for VinoVista: VIN Insight 🚗✨. This policy outlines the types of data we collect from our users, how we use it, and the steps we take to protect it.</p>
+                    <h2 className="text-xl font-semibold mb-4">1. Acceptance of Terms</h2>
+                    <p>By using VinoVista: VIN Insight, you agree to be bound by these terms. If you do not agree with these terms, please do not use the add-on.</p>
                 </section>
-
-                {/* ... Add other sections similarly ... */}
 
                 <section className="mb-8">
-                    <h2 className="text-xl font-semibold mb-4">8. Contact Us</h2>
-                    <p>If you have questions about this Privacy Policy or our data practices, please contact:</p>
-                    <ul className="list-disc pl-5 mt-2">
-                        <li>Developer: Arnold Puidokas</li>
-                        <li>Email: arnaspuidokas6@gmail.com</li>
-                    </ul>
+                    <h2 className="text-xl font-semibold mb-4">2. Changes to Terms</h2>
+                    <p>We reserve the right to modify or replace these terms at any time. Any changes will be posted here, and by continuing to use the add-on after changes are made, you agree to be bound by the updated terms.</p>
                 </section>
 
+                <section className="mb-8">
+                    <h2 className="text-xl font-semibold mb-4">3. Use of the Add-on</h2>
+                    <p>You agree not to misuse the add-on or help anyone else to do so. You also agree to use the add-on only for lawful purposes.</p>
+                </section>
+
+                <section className="mb-8">
+                    <h2 className="text-xl font-semibold mb-4">4. Termination</h2>
+                    <p>We reserve the right to terminate or suspend your access to the add-on without prior notice if you violate any of the terms outlined here.</p>
+                </section>
+
+                <section className="mb-8">
+                    <h2 className="text-xl font-semibold mb-4">5. Disclaimers</h2>
+                    <p>The add-on is provided &quot;as is&quot; without any warranties, express or implied. We do not guarantee that the add-on will be available at all times or that it will function without errors.</p>
+                </section>
+
+                <section className="mb-8">
+                    <h2 className="text-xl font-semibold mb-4">6. Limitation of Liability</h2>
+                    <p>We shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss arising from the use of the add-on.</p>
+                </section>
+
+                <section className="mb-8">
+                    <h2 className="text-xl font-semibold mb-4">7. Governing Law</h2>
+                    <p>These terms are governed by the laws of Lithuania. Any disputes arising from these terms will be resolved in the courts of Lithuania.</p>
+                </section>
+
+                <section className="mb-8">
+                    <h2 className="text-xl font-semibold mb-4">8. Contact Information</h2>
+                    <p>For any questions or concerns regarding these terms, please contact Arnold Puidokas at arnaspuidokas6@gmail.com.</p>
+                </section>
             </div>
         </div>
     );
 }
 
-export default PrivacyPolicy;
+export default TermsOfService;
