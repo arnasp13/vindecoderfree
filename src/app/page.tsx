1df6aa8d5bde6136cbc8917b9287e41166af4a62
Arnas <arnaspuidokas6@gmail.com>
2023-11-02 15:06:19 +0200

    add links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,77 +45,83 @@ export default function Home() {
         </div>
       </section>
 
-      {/* <div className="py-12">
+      <div className="bg-secondary-2 py-12">
         <div>
-          <section className="bg-white dark:bg-gray-900">
+          <section>
             <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
               <div className="max-w-screen-md mb-8 lg:mb-16">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
                   What are we offering
                 </h2>
               </div>
-              <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+              <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-8 md:space-y-0">
                 <div>
-                  <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <div className="flex justify-left items-center mb-2 w-14 h-14 rounded-full bg-primary-100">
                     <Image
-                      src="/icons/book.svg"
+                      src="/icons/computer.svg"
                       width={30}
                       height={30}
-                      alt="Book Icon"
+                      alt="Computer Icon"
                     />
                   </div>
                   <h3 className="mb-2 text-xl font-bold dark:text-white">
-                    Website Integration
+                    VIN Decoder Website
                   </h3>
                   <p className="text-gray-500 dark:text-gray-400">
-                    Let us provide the best data sources for you.
+                    Access our free website for comprehensive vehicle details
+                    with just a VIN.{" "}
                   </p>
                 </div>
                 <div>
-                  <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <div className="flex justify-left items-center mb-2 w-14 h-14 rounded-full bg-primary-100">
                     <Image
-                      src="/icons/details.svg"
+                      src="/icons/google-sheets.svg"
                       width={30}
                       height={30}
-                      alt="Details Icon"
+                      alt="Google Sheets Icon"
                     />
                   </div>
                   <h3 className="mb-2 text-xl font-bold dark:text-white">
                     Google Sheets Add-On
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">
-                    Input a VIN and retrieve the vehicle&apos;s Year, Make,
-                    Model, and Trims directly from the manufacturer.
+                  <p className="text-gray-500 mb-3">
+                    Integrate directly into your spreadsheets to fetch the
+                    vehicle&apos;s Year, Make, Model, and Trims from the
+                    manufacturer.
                   </p>
+                  <Link href="/google-sheets">Get Google Sheets Add-on</Link>
                 </div>
                 <div>
-                  <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <div className="flex justify-left items-center mb-2 w-14 h-14 rounded-full bg-primary-100">
                     <Image
-                      src="/icons/car.svg"
+                      src="/icons/chrome.svg"
                       width={30}
                       height={30}
-                      alt="Car Icon"
+                      alt="Chrome Icon"
                     />
                   </div>
                   <h3 className="mb-2 text-xl font-bold dark:text-white">
                     Chrome Extension
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">
-                    Obtain comprehensive descriptions and codes of vehicle
-                    features as provided by the original equipment manufacturer
-                    (OEM).
+                  <p className="text-gray-500 mb-3">
+                    Enhance your browser to access comprehensive vehicle
+                    details, from descriptions and codes of features to general
+                    information, all sourced directly from the original
+                    equipment manufacturer (OEM).
                   </p>
+                  <Link href="/https://chrome.google.com/webstore/detail/vindecoderfreecom/ckghkkfgljadklgemfelfckaiimbnjki">
+                    Get Chrome Extension
+                  </Link>
                 </div>
               </div>
             </div>
           </section>
         </div>
-      </div> */}
-
+      </div>
 
       <div className="py-12">
         <div>
-          <section className="bg-white dark:bg-gray-900">
+          <section>
             <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
               <div className="max-w-screen-md mb-8 lg:mb-16">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
